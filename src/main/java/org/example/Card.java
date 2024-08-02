5773c25455aea0d8f18639720f2dfc8403b4f42a
Karan Ahuja <karan.ahuja.ext@atomtech.in>
2024-08-02 20:53:08 +0530

    add code

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import static org.example.Fsrs.WEIGHTS;
+
 public class Card{
+
+    float initialStability(int gradeNumber) {
+        return WEIGHTS[gradeNumber - 1];
+    }
+
+    float initialDifficulty(int gradeNumber){
+        final float calculatedValue = WEIGHTS[4] + (gradeNumber - 3) * WEIGHTS[5];
+        return Math.max(1,Math.min(10, calculatedValue));
+    }
 
 }
--- src/main/java/org/example/Fsrs.java
+++ src/main/java/org/example/Fsrs.java
@@ -9,6 +9,12 @@ public class Fsrs {
             Map.entry("GOOD", 3),
             Map.entry("EASY", 4)
     );
-    static final float DECAY = -0.5;
+    static final float DECAY = -0.5f;
+    static final float REQUESTED_RETENTION_RATE = 0.9f;
+    static final float WEIGHTS[] = new float[] {0.4f, 0.6f, 2.4f, 5.8f, 4.93f, 0.94f, 0.86f, 0.01f, 1.49f, 0.14f, 0.94f, 2.18f, 0.05f,
+            0.34f, 1.26f, 0.29f, 2.61f};
+
+    static final float FACTOR = 19 / 81;
+
 
 }
